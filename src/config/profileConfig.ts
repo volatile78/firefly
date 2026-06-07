972766a718ca1d398c7df28e73a899e977770564
volatile78 <550734217@qq.com>
2026-06-07 10:23:43 +0800

    added a post

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "https://i0.hdslb.com/bfs/face/2d599cc413584941b1846c7bf308de502d1992a3.jpg@240w_240h_1c_1s_!web-avatar-nav.avif",
+	avatar: "assets/images/avatar.avif",
 
 	// 名字
 	name: "volatile",
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "https://cdn.luogu.com.cn/upload/image_hosting/690g6uk9.png",
+			src: "assets/images/avatar.avif",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
@@ -74,8 +74,8 @@ export const siteConfig: SiteConfig = {
 		// 3. 本地图片（src目录，自动优化但会增加构建时间，推荐）: { type: "image", value: "assets/images/logo.webp", alt: "Logo" }
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
-			type: "url",
-			value: "https://cdn.luogu.com.cn/upload/image_hosting/690g6uk9.png",
+			type: "image",
+			value: "assets/images/avatar.avif",
 			alt: "Logo",
 		},
 		// 导航栏标题
